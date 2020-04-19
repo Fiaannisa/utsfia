d72008c5fa11b5ab5d88b1f3ea20aa9ae75c21b5
fiaannisa <annisafia06@gmail.com>
2020-04-19 11:27:40 +0700

    membuat file Controller

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req, res){
-    response.ok("Project UTS Rest API berjalan !")
+    response.ok("Project UTS Rest API berjalan!",res)
 };
